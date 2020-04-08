6b3eaa7cd8d819ec5edcc293603147cad5eac245
Hung Dinh Nguyen <hungnguyendinh1999@gmail.com>
2020-04-08 21:07:56 +0700

    Remove Test

--- HackAssembler/main.c
+++ HackAssembler/main.c
@@ -16,7 +16,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hung's random shit\n");
     openFile("hello.asm");
     FILE *fp = fopen("hello.asm", "r");
     //char* temp= (char*)malloc(20*sizeof(char));
